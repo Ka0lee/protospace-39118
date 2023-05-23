aaa0acd0951f28386799e71e4fe9e3bfebcb6f98
Ka0lee <pekochan90@gmail.com>
2023-05-23 23:13:07 +0900

    ストロングパラメータの設定・ビューの修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,4 +1,6 @@
 class PrototypesController < ApplicationController
+  before_action :move_to_index, except: [:index]
+  
   def index
   end
 
@@ -6,5 +8,31 @@ class PrototypesController < ApplicationController
     @user = user.new
   end
 
+  def edit
+    
+  end
+
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      # 保存成功時の処理
+      redirect_to @user
+    else
+      # 保存失敗時の処理
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :encrypted_password, :password_confirmation)
+  end
   
+  def move_to_index
+    unless user_signed_in?
+      redirect_to action: :index
+    end
+  end
 end
