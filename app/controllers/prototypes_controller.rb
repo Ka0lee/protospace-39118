a22e25b4545a4b0796da203fb84198b7f8493860
Ka0lee <pekochan90@gmail.com>
2023-05-30 22:03:10 +0900

    削除機能の実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,41 +3,57 @@ class PrototypesController < ApplicationController
 
 
   def index
-    @prototypes=Prototype.all
+    @prototypes=Prototype.includes(:user)
   end
 
   def new
-    @user = User.new
     @prototype = Prototype.new
   end
 
   def create
-    @prototype = current_user.prototypes.build(prototype_params)
+    @prototype = current_user.prototype.build(prototype_params)
 
     if @prototype.save
- 
       redirect_to root_path  # 保存成功時の処理
     else
       render :new #保存失敗時の処理
     end
   end
 
-  def destroy
-    @Prototype.find(params[:id])
-    @prototype.destroy
-    redirect_to prototypes_path
+def show
+   @prototype = Prototype.find(params[:id]) 
+end
+
+def edit
+  @prototype = Prototype.find(params[:id]) 
+end
+
+def update
+  @prototype = Prototype.find(params[:id])
+
+  if @prototype.update(prototype_params)
+    redirect_to root_path
+  else
+    render :edit
   end
+end
+
+def destroy
+  @prototype = Prototype.find(params[:id])
+  @prototype.destroy
+  redirect_to prototypes_path
+end
   
   private
-
-  def prototype_params
-    params.require(:prototype).permit(:title, :catch_copy, :concept, :image,).merge(user_id: current_user.id)
-  end
-
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
+    def prototype_params
+      params.require(:prototype).permit(:title, :catch_copy, :concept, :image,).merge(user_id: current_user.id)
     end
-  end
+
+    def move_to_index
+      unless user_signed_in?
+        redirect_to action: :index
+      end
+    end
   
-end
+  end  
+
