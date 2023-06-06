8f9f2df1c2f0f8c618c9bd6006cdd3644ba824a8
Ka0lee <pekochan90@gmail.com>
2023-06-06 23:42:54 +0900

    ログイン状態によってページ遷移先を変更

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,7 @@
 class PrototypesController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
-
+  before_action :authenticate_user!,except:[:index,:show,:new]
+  before_action :set_prototype,only:[:show,:edit,:update,:destroy]
+  before_action :contributor_confirmation, only: [:edit, :update,:destroy]
 
   def index
     @prototypes=Prototype.includes(:user)
@@ -21,19 +22,16 @@ class PrototypesController < ApplicationController
   end
 
 def show
-   @prototype = Prototype.find(params[:id]) 
    @comment = Comment.new
    @comments = @prototype.comments.includes(:user)
 
 end
 
 def edit
-  @prototype = Prototype.find(params[:id]) 
+
 end
 
 def update
-  @prototype = Prototype.find(params[:id])
-
   if @prototype.update(prototype_params)
     redirect_to root_path
   else
@@ -42,21 +40,26 @@ def update
 end
 
 def destroy
-  @prototype = Prototype.find(params[:id])
   @prototype.destroy
   redirect_to prototypes_path
 end
   
+
   private
+
     def prototype_params
       params.require(:prototype).permit(:title, :catch_copy, :concept, :image,).merge(user_id: current_user.id)
     end
 
-    def move_to_index
-      unless user_signed_in?
-        redirect_to action: :index
-      end
+    def set_prototype
+      @prototype = Prototype.find(params[:id]) 
     end
+
+
+    def contributor_confirmation
+      redirect_to root_path unless @prototype.user == current_user
+    end
+   
   
   end  
 
