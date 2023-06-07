6b007ba63796bc8621f57671d924ca9463747081
Ka0lee <pekochan90@gmail.com>
2023-06-07 21:39:04 +0900

    updateアクションへのルーティング再設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'prototypes#index'
 
-  resources :prototypes ,only:[:index,:new,:create,:show,:edit,:destroy]do
+  resources :prototypes ,only:[:index,:new,:create,:show,:edit,:destroy,:update]do
     resources :comments, only: :create
   end
   resources :users, only: [:show,:index]
